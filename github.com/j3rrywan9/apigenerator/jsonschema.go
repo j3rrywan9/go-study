efefb22ad9b943fe9aeee286f15bbab25249cbb6
Jerry Wang <jerry.wang@delphix.com>
2015-04-08 14:02:34 -0700

    update apigenerator/jsonschema.go

--- github.com/j3rrywan9/apigenerator/jsonschema.go
+++ github.com/j3rrywan9/apigenerator/jsonschema.go
@@ -1,21 +1,46 @@
 package apigenerator
 
-type JSONSchema struct {
+type JSONSchemaFile struct {
 	name string
 	content map[string]interface{}
 }
 
-func  NewJSONSchema(name string, content map[string]interface{}) *JSONSchema {
-	c := &JSONSchema{name, content}
-	return c
+type JSONSchema struct {
+	name string
+	title string
+	description string
+	required bool
+	enum []string 
+	minimum uint16
+	maximum uint16
+	exclusive_minimum uint16
+	exclusive_maximum uint16
+	divisible_by uint16
+	default_value string
+	ref string
+	pattern string
+	max_length uint16
+	min_length uint16
+	format string
+	items map[string]string
+	value_type string
+	properties map[string]map[string]string
 }
 
-func (js *JSONSchema) Name() string {
-	return js.name
+func (this *JSONSchemaFile) Name() string {
+	return this.name
 }
 
-func (js *JSONSchema) Content() map[string]interface{} {
-	return js.content
+func (this *JSONSchemaFile) SetName(name string) {
+	this.name = name
+}
+
+func (this *JSONSchemaFile) Content() map[string]interface{} {
+	return this.content
+}
+
+func (this *JSONSchemaFile) SetContent(content map[string]interface{}) {
+	this.content = content
 }
 
 type JSONSchemaExtended struct {
