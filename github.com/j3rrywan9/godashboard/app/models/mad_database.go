4f03774f65bf04d4f93d68606e831dcbbe56fb7f
Jerry Wang <jerry.wang@delphix.com>
2015-06-18 23:46:27 -0700

    rename godashboard/app/models/models.go to mad_platform.go add godashboard/app/models/mad_database.go

---
+++ github.com/j3rrywan9/godashboard/app/models/mad_database.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+)
+
+type Mad_database struct {
+	Id int
+	Vendor string `sql:"size:50"`
+	Version string `sql:"size:50"`
+}
+
+func Get_all_databases() []Mad_database {
+	sqlConnection := "user=postgres dbname=postgres password=postgres host=10.43.0.157 port=5432 sslmode=disable"
+	db, err := gorm.Open("postgres", sqlConnection)
+	if err != nil {
+		panic(err)
+	}
+	//db.SingularTable(true)
+	var records []Mad_database
+	db.Table("mad_database").Select("*").Scan(&records)
+	return records
+}
