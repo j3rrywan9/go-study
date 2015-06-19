d5adf17430adc8db201101112fdb7122585f9ab4
Jerry Wang <jerry.wang@delphix.com>
2015-06-18 21:41:29 -0700

    update godashboard

--- github.com/j3rrywan9/godashboard/app/controllers/app.go
+++ github.com/j3rrywan9/godashboard/app/controllers/app.go
@@ -14,8 +14,15 @@ func (c App) Index() revel.Result {
 }
 
 func (c App) Platform() revel.Result {
-	var MyPlatform []models.Mad_platform
-	MyPlatform = models.Get_all_platforms()
-	return c.Render(MyPlatform)
+	var myPlatform []models.Mad_platform
+	myPlatform = models.Get_all_platforms()
+	return c.Render(myPlatform)
+}
+
+
+func (c App) Database() revel.Result {
+	var myDatabase []models.Mad_database
+	myDatabase = models.Get_all_databases()
+	return c.Render(myDatabase)
 }
 
--- github.com/j3rrywan9/godashboard/app/models/models.go
+++ github.com/j3rrywan9/godashboard/app/models/models.go
@@ -10,6 +10,12 @@ type Mad_platform struct {
 	Name string `sql:"size:50"`
 }
 
+type Mad_database struct {
+	Id int
+	Vendor string `sql:"size:50"`
+	Version string `sql:"size:50"`
+}
+
 func Get_all_platforms() []Mad_platform {
 	sqlConnection := "user=postgres dbname=postgres password=postgres host=10.43.0.157 port=5432 sslmode=disable"
 	db, err := gorm.Open("postgres", sqlConnection)
@@ -19,6 +25,18 @@ func Get_all_platforms() []Mad_platform {
 	db.SingularTable(true)
 	var records []Mad_platform
 	db.Table("mad_platform").Select("*").Scan(&records)
+	return records
+}
+
+func Get_all_databases() []Mad_database {
+	sqlConnection := "user=postgres dbname=postgres password=postgres host=10.43.0.157 port=5432 sslmode=disable"
+	db, err := gorm.Open("postgres", sqlConnection)
+	if err != nil {
+		panic(err)
+	}
+	//db.SingularTable(true)
+	var records []Mad_database
+	db.Table("mad_database").Select("*").Scan(&records)
 	return records
 }
 
