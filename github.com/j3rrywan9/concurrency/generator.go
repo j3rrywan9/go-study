e609b5849b5007040ff7ad21f06a7630aafdfaf7
Jerry Wang <jerry.wang@delphix.com>
2015-06-28 09:58:03 -0700

    add concurrency/generator.go

---
+++ github.com/j3rrywan9/concurrency/generator.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Generator: function that returns a channel
+func boring(msg string) <-chan string {	// Returns receive-only channel of strings.
+	ch := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			ch <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return ch
+}
+
+func main() {
+	joe := boring("Joe")
+	ann := boring("Ann")
+	for i := 0; i < 5; i++ {
+		fmt.Println(<-joe)
+		fmt.Println(<-ann)
+	}
+	fmt.Println("You're both boring; I'm leaving.")
+}
