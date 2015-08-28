9478d17f8062a196440205b808ccaf634d6ea149
Jerry Wang <jerry.wang@delphix.com>
2015-08-28 10:43:59 -0700

    update algorithms/bubble_sort.go

--- github.com/j3rrywan9/algorithms/bubble_sort.go
+++ github.com/j3rrywan9/algorithms/bubble_sort.go
@@ -6,12 +6,17 @@ import (
 
 func bubbleSort(array []int) {
 	for k := 1; k < len(array); k++ {
+		flag := 0
 		for i := 0; i < len(array) - 1; i++ {
 			if array[i] > array[i + 1] {
 				temp := array[i + 1]
 				array[i + 1] = array[i]
 				array[i] = temp
+				flag = 1
 			}
+		}
+		if flag == 0 {
+			break
 		}
 	}
 }
