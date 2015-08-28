703196fe1fee794ce3708932ea4bbb3d6e3b17b1
Jerry Wang <jerry.wang@delphix.com>
2015-08-28 09:55:03 -0700

    update algorithms/selection_sort.go

--- github.com/j3rrywan9/algorithms/selection_sort.go
+++ github.com/j3rrywan9/algorithms/selection_sort.go
@@ -5,6 +5,7 @@ import (
 )
 
 func selectionSort(array []int) {
+	// We only need to run this loop len(array) - 2 times
 	for i := 0; i < len(array) - 1; i++ {
 		min := i
 		for j := i + 1; j < len(array); j++ {
@@ -12,6 +13,7 @@ func selectionSort(array []int) {
 				min = j
 			}
 		}
+		// Swap array[i] and array[min]
 		temp := array[i]
 		array[i] = array[min]
 		array[min] = temp
